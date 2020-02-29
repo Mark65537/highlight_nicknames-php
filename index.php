f8e474fa8f8ece2b6c0e4b8e2829901ad6119018
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2020-02-29 15:06:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,11 +10,9 @@
   <?php
 
   function highlight_nicknames(string $text):string{
-     $pattern = '/@\w+[^\S.]/';
+     $pattern = '/(?:^|\s)@\w+[^\S.]/';
      preg_match_all($pattern, $text, $matches);
-     if (empty($matches)) {
-         $text="ни один Ник не найден в данной строке";
-      } else {
+     if (!empty($matches))  {
          foreach($matches[0] as $match){
              $pos=strpos($text,$match);
              $text=substr_replace($text,'<b>',$pos,0);
@@ -25,7 +23,7 @@
       return $text;
   }
 
-  echo highlight_nicknames("@bnm сообщил нам вчера о результатах @usernick; ");
+  echo highlight_nicknames("@bnm сообщил нам вчера о результатах \n@usernick ");
   ?>
   </body>
 </html>
